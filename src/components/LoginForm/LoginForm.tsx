63996461c553afbcd0559a979241eefb81431753
GiovannyRamirez <giovannyramirezs@hotmail.com>
2024-03-01 11:44:52 -0500

    Add Register and Login Forms components

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,37 @@
+import { useForm } from "../../hooks/useForm";
+
+import { Button } from "../Button/Button";
+import { Input } from "../Input/Input";
+
+import "./styles.css";
+
+export const LoginForm = () => {
+  const { form, email, password, onChange } = useForm({
+    email: "",
+    password: "",
+  });
+
+  const onSubmit = () => {
+    console.log(form);
+  };
+
+  return (
+    <div className="formContent">
+      <h3 className="loginTitle">Iniciar sesión</h3>
+      <Input
+        id="emailInput"
+        label="Correo electrónico"
+        value={email}
+        onChange={({ target }) => onChange(target.value, "email")}
+      />
+      <Input
+        id="passwordInput"
+        label="Contraseña"
+        type="password"
+        value={password}
+        onChange={({ target }) => onChange(target.value, "password")}
+      />
+      <Button label="Iniciar sesión" onClick={onSubmit} />
+    </div>
+  );
+};
---
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,52 @@
+import { useForm } from "../../hooks/useForm";
+
+import { Button } from "../Button/Button";
+import { Input } from "../Input/Input";
+
+import "./styles.css";
+
+export const RegisterForm = () => {
+  const { form, name, email, password, confirmPassword, onChange } = useForm({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const onSubmit = () => {
+    console.log(form);
+  };
+
+  return (
+    <div className="formContent">
+      <h3 className="registerTitle">Regístrate</h3>
+      <Input
+        id="nameInput"
+        label="Nombre"
+        value={name}
+        onChange={({ target }) => onChange(target.value, "name")}
+      />
+      <Input
+        id="emailInput"
+        label="Correo electrónico"
+        value={email}
+        onChange={({ target }) => onChange(target.value, "email")}
+      />
+      <Input
+        id="passwordInput"
+        label="Contraseña"
+        type="password"
+        value={password}
+        onChange={({ target }) => onChange(target.value, "password")}
+      />
+      <Input
+        id="confirmPasswordInput"
+        label="Confirma contraseña"
+        type="password"
+        value={confirmPassword}
+        onChange={({ target }) => onChange(target.value, "confirmPassword")}
+      />
+      <Button label="Registrarme" onClick={onSubmit} />
+    </div>
+  );
+};
