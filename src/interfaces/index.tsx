5bf4f67160f9f7f8e8904dd096583775dd597453
GiovannyRamirez <giovannyramirezs@hotmail.com>
2024-03-02 14:20:40 -0500

    Add Request/Responses interfaces

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -1,8 +1,40 @@
-export interface IAuthResponse {
+export interface IBasicResponse {
   error: boolean;
-  email: string;
   message: string;
+}
+export interface IAuthLoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface IAuthRegisterRequest extends IAuthLoginRequest {
+  name: string;
+}
+
+export interface IAuthResponse extends IBasicResponse {
+  email: string;
   name: string;
   userId: number;
   token: string;
+}
+
+export interface IProductProps {
+  name: string;
+  description: string;
+  price: string;
+}
+
+export interface IProduct extends IProductProps {
+  id_product: number | string;
+  id_user?: number | string;
+}
+
+export interface IProductsResponse extends IBasicResponse {
+  error: boolean;
+  message: string;
+  products: IProduct[];
+}
+
+export interface IProductResponse extends IBasicResponse {
+  productInfo: IProduct;
 }
