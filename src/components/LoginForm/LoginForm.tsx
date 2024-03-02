233a0bfdb98fe110f8d29d5df9a8e57ea11b8160
GiovannyRamirez <giovannyramirezs@hotmail.com>
2024-03-01 19:24:51 -0500

    Add auth backend integration

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,22 +1,57 @@
-import { useForm } from "../../hooks/useForm";
+import { useState } from "react";
 
+import { IAuthResponse } from "../../interfaces/index";
+
+import productsApi from "../../api";
+import { useForm } from "../../hooks/useForm";
+import { ENDPOINTS, GET_ERROR } from "../../constants";
+
+import { Loader } from "../Loader/Loader";
 import { Button } from "../Button/Button";
 import { Input } from "../Input/Input";
+import { Modal } from "../Modal/Modal";
 
 import "./styles.css";
 
 export const LoginForm = () => {
-  const { form, email, password, onChange } = useForm({
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<boolean | string>("");
+
+  const { email, password, onChange } = useForm({
     email: "",
     password: "",
   });
 
-  const onSubmit = () => {
-    console.log(form);
+  const onSubmit = async () => {
+    setLoading(true);
+    try {
+      const { data } = await productsApi.post<IAuthResponse>(ENDPOINTS.LOGIN, {
+        email,
+        password,
+      });
+
+      const userToSave = JSON.stringify({
+        name: data.name,
+        email: data.email,
+        userId: data.userId,
+      });
+
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("user", userToSave);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      const errorMessage = GET_ERROR(err?.response?.data?.error);
+      setError(errorMessage);
+    } finally {
+      setLoading(false);
+    }
   };
+
+  const isButtonDisabled = !email || !password;
 
   return (
     <div className="formContent">
+      <Loader loading={loading} />
       <h3 className="loginTitle">Iniciar sesión</h3>
       <Input
         id="emailInput"
@@ -31,7 +66,16 @@ export const LoginForm = () => {
         value={password}
         onChange={({ target }) => onChange(target.value, "password")}
       />
-      <Button id="loginButton" label="Iniciar sesión" onClick={onSubmit} />
+      <Button
+        disabled={isButtonDisabled}
+        id="loginButton"
+        label="Iniciar sesión"
+        onClick={onSubmit}
+      />
+
+      <Modal open={Boolean(error)} onClose={() => setError(false)}>
+        <h3 className="error">{error}</h3>
+      </Modal>
     </div>
   );
 };
--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,24 +1,73 @@
-import { useForm } from "../../hooks/useForm";
+import { useState } from "react";
 
+import { IAuthResponse } from "../../interfaces";
+
+import productsApi from "../../api";
+import { useForm } from "../../hooks/useForm";
+import { ENDPOINTS, GET_ERROR, STATUS } from "../../constants";
+
+import { Loader } from "../Loader/Loader";
 import { Button } from "../Button/Button";
 import { Input } from "../Input/Input";
+import { Modal } from "../Modal/Modal";
 
 import "./styles.css";
 
 export const RegisterForm = () => {
-  const { form, name, email, password, confirmPassword, onChange } = useForm({
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<boolean | string>("");
+
+  const { name, email, password, confirmPassword, onChange } = useForm({
     name: "",
     email: "",
     password: "",
     confirmPassword: "",
   });
 
-  const onSubmit = () => {
-    console.log(form);
+  const onSubmit = async () => {
+    setLoading(true);
+    try {
+      const { data, status } = await productsApi.post<IAuthResponse>(
+        ENDPOINTS.REGISTER,
+        {
+          name,
+          email,
+          password,
+        }
+      );
+
+      if (status === STATUS.ACCEPTED) {
+        const errorMessage = GET_ERROR(String(data.error));
+        setError(errorMessage);
+      } else {
+        const userToSave = JSON.stringify({
+          name: data.name,
+          email: data.email,
+          userId: data.userId,
+        });
+
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", userToSave);
+      }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      const errorMessage = GET_ERROR(err?.response?.data?.error);
+      setError(errorMessage);
+    } finally {
+      setLoading(false);
+    }
   };
+
+  const isButtonDisabled =
+    !name ||
+    !email ||
+    !password ||
+    !confirmPassword ||
+    password !== confirmPassword;
 
   return (
     <div className="formContent">
+      <Loader loading={loading} />
       <h3 className="registerTitle">Regístrate</h3>
       <Input
         id="nameInput"
@@ -46,7 +95,16 @@ export const RegisterForm = () => {
         value={confirmPassword}
         onChange={({ target }) => onChange(target.value, "confirmPassword")}
       />
-      <Button id="registerButton" label="Registrarme" onClick={onSubmit} />
+      <Button
+        disabled={isButtonDisabled}
+        id="registerButton"
+        label="Registrarme"
+        onClick={onSubmit}
+      />
+
+      <Modal open={Boolean(error)} onClose={() => setError(false)}>
+        <h3 className="error">{error}</h3>
+      </Modal>
     </div>
   );
 };
