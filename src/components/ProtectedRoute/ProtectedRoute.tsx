d76857a2e7b83bf551bae7d1ff7481e580be5092
GiovannyRamirez <giovannyramirezs@hotmail.com>
2024-03-02 15:07:48 -0500

    Connect ProfileNav in ProtectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -2,10 +2,17 @@ import { Navigate, Outlet } from "react-router-dom";
 
 import { URL_PAGES } from "../../constants";
 
+import { ProfileNav } from "../ProfileNav/ProfileNav";
+
 export const ProtectedRoute = () => {
   const token = localStorage.getItem("token");
 
   if (!token) return <Navigate to={URL_PAGES.WELCOME} />;
 
-  return <Outlet />;
+  return (
+    <>
+      <ProfileNav />
+      <Outlet />
+    </>
+  );
 };
